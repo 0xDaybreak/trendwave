ee5b954bfb8e174d05ddfd47af609a3569703e24
Daybreak <andrei1.popescu1@gmail.com>
2023-04-10 18:26:00 +0300

    added infinite scroll

--- frontend/components/homeComponents/CardHolder.tsx
+++ frontend/components/homeComponents/CardHolder.tsx
@@ -2,7 +2,7 @@ import Card from './Card';
 import './CardHolder.css';
 import { HorizontalLayout } from '@hilla/react-components/HorizontalLayout.js';
 import { VerticalLayout } from '@hilla/react-components/VerticalLayout.js';
-import { useEffect, useState, useRef } from 'react';
+import {useEffect, useState, useRef, useCallback} from 'react';
 import { VideoEntityEndpoint } from 'Frontend/generated/endpoints';
 import VideoEntity from 'Frontend/generated/com/video/application/entity/VideoEntity';
 
@@ -12,26 +12,38 @@ const CardHolder = () => {
     const [entityChunks, setEntityChunks] = useState<VideoEntity[][]>([]);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth * 0.8 / 4);
     const [cardsPerRow, setCardsPerRow] = useState(4);
-    const myRef:any = useRef();
-    const[myElementIsVisible, setMyElementISVisible] = useState<any>();
+    const [pageNr, setPageNr] = useState(0);
 
+    const myRef:any = useRef(null);
+    const lastCard:any = useCallback(
+        (node:any) => {
+            if(!node) return;
+            if(myRef.current) myRef.current.disconnect();
+            myRef.current = new IntersectionObserver((entries) => {
+                if(entries[0].isIntersecting) {
+                    console.log("I am intersecting")
+                    setPageNr((prevPageNr) => prevPageNr + 1);
+                }
+            },{ threshold: 0.88 });
+            myRef.current.observe(node);
+        }, [1]
+    );
     let cardCounter = 1;
 
     useEffect(() => {
-        const handleResize = () => setWindowWidth(window.innerWidth * 0.8 / 4);
+        const handleResize = () => {setWindowWidth(window.innerWidth * 0.8 / 4)};
         window.addEventListener("resize", handleResize);
+
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
 
     useEffect(() => {
-        VideoEntityEndpoint.findTwelve(0).then(setVEntities);
-        myRef.current = new IntersectionObserver(row => {
-            if(row[0].isIntersecting) {
-                console.log("we are intersecting")
-            }
-        })
-    }, []);
+        console.log(pageNr)
+        VideoEntityEndpoint.findTwelve(pageNr).then((newVEntities) =>
+            setVEntities((prevVEntities) => [...prevVEntities, ...newVEntities])
+        );
+    }, [pageNr]);
 
     useEffect(() => {
         const calculateCardsPerRow = () => {
@@ -61,15 +73,15 @@ const CardHolder = () => {
     return (
         <VerticalLayout className="card-holder-vertical-layout">
             {entityChunks.map((chunk, index) => (
-                <div key={index} style={{ marginBottom: '20px' }}>
+                <div key={index} className='margin-bottom'>
                     {cardsPerRow === 1 ? (
-                        <VerticalLayout>
+                        <VerticalLayout ref={lastCard}>
                             {chunk.map((entity) => (
                                 <Card url={entity.url} key={cardCounter++} id={entity.id}/>
                             ))}
                         </VerticalLayout>
                     ) : (
-                        <HorizontalLayout ref={cardCounter===12 ? myRef:null}>
+                        <HorizontalLayout ref={lastCard}>
                             {chunk.map((entity) => (
                                 <Card width={windowWidth} url={entity.url} key={cardCounter++} id={entity.id}/>
                             ))}

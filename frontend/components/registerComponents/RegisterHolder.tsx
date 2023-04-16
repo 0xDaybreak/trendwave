fb5f97fba9d4aa46a9149fe2c28b1bd9985d82ed
Deiu <andrei1.popescu1@gmail.com>
2023-04-16 16:42:06 +0300

    ui changes

--- frontend/components/registerComponents/RegisterHolder.tsx
+++ frontend/components/registerComponents/RegisterHolder.tsx
@@ -1,23 +1,25 @@
-import {VerticalLayout} from "@hilla/react-components/VerticalLayout.js";
+import { VerticalLayout } from "@hilla/react-components/VerticalLayout.js";
 import React from "react";
-import {Button} from "@hilla/react-components/Button.js";
-import {TextField} from "@hilla/react-components/TextField.js";
-import '@vaadin/vaadin-lumo-styles/utility.js';
-import './RegisterHolder.css';
+import { Button } from "@hilla/react-components/Button.js";
+import { TextField } from "@hilla/react-components/TextField.js";
+import "@vaadin/vaadin-lumo-styles/utility.js";
+import "./RegisterHolder.css";
+import sheepImg from 'Frontend/images/sheep.png';
+
 const RegisterHolder = () => {
     return (
-    <VerticalLayout className="card-holder-vertical-layout">
-        <div className={"m-auto"}>
-            <VerticalLayout className={"min-h-screen"}>
-                <TextField className={"text"}>Email</TextField>
-                <TextField>Password</TextField>
-                <TextField>Repeat Password</TextField>
-                <Button className={"text"}> Create Account</Button>
-            </VerticalLayout>
-        </div>
-    </VerticalLayout>
+        <VerticalLayout className="card-holder-vertical-layout">
+            <div className={"m-auto"}>
+                <VerticalLayout className={"min-h-screen"}>
+                    <img alt="logo" src={sheepImg} />
+                    <TextField className={"text"}>Email</TextField>
+                    <TextField>Password</TextField>
+                    <TextField>Repeat Password</TextField>
+                    <Button className={"text"}> Create Account</Button>
+                </VerticalLayout>
+            </div>
+        </VerticalLayout>
     );
-
-}
+};
 
 export default RegisterHolder;

f756852a9e81e519071d400f17802eac14bc0f26
Daybreak <andrei1.popescu1@gmail.com>
2023-05-07 19:54:15 +0300

    audio combined

--- frontend/components/homeComponents/Card.tsx
+++ frontend/components/homeComponents/Card.tsx
@@ -6,7 +6,7 @@ import { Button } from "@hilla/react-components/Button.js";
 
 interface CardProps {
     key?:number
-    url?:string;
+    url?:any;
     audio?:any;
     width?:number;
     id?:string;
@@ -21,22 +21,30 @@ const Card:React.FC<CardProps> = (props:CardProps) => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const [isNewVideo, setIsNewVideo] = useState(false);
 
-    const handleMouseEnter = () => {
-        if (videoRef.current) {
-            videoRef.current.play();
-            videoRef.current.style.opacity = '100%';
-        }
-    }
+    useEffect(() => {
+        const video = videoRef.current;
 
-    const handleMouseLeave = () => {
-        if (videoRef.current) {
-            videoRef.current.pause();
-            videoRef.current.style.opacity = '70%';
-            if(videoRef.current.currentTime===videoRef.current.duration) {
-                //videoRef.current.style.opacity = '100%';
-            }
+        // Create a MediaSource object
+        const mediaSource = new MediaSource();
+
+        // Set up the video element to use the MediaSource object as its source
+        if(video!=null) {
+        video.src = URL.createObjectURL(mediaSource);
         }
-    }
+
+        // When the MediaSource object is ready, create a SourceBuffer and append the video and audio sources
+        mediaSource.addEventListener('sourceopen', () => {
+            const sourceBuffer = mediaSource.addSourceBuffer('video/mp4; codecs="avc1.4d401f"');
+            fetch(props.url)
+                .then((response) => response.arrayBuffer())
+                .then((buffer) => sourceBuffer.appendBuffer(buffer));
+
+            const audioSourceBuffer = mediaSource.addSourceBuffer('audio/mp4; codecs="mp4a.40.2"');
+            fetch(props.audio)
+                .then((response) => response.arrayBuffer())
+                .then((buffer) => audioSourceBuffer.appendBuffer(buffer));
+        });
+    }, [props.url, props.audio]);
 
     const checkIsNewVideo = async () => {
         const isNew = await VideoEntityEndpoint.isNew(props.id);
@@ -60,10 +68,7 @@ const Card:React.FC<CardProps> = (props:CardProps) => {
     return (
         <div style={{width:props.width}} className="card-item" >
             {isNewVideo && <Button disabled className="new-video">New</Button>}
-            <video className="video-insert" muted disablePictureInPicture controlsList="nodownload" ref={videoRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <source src={props.url} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <video ref={videoRef} className={"video-insert"} controls />
             <CardStatusBar id={props.id} subreddit={props.subreddit} tags={props.tags} post={props.post} isFavourite={props.isFavourite} onFavouriteNotLoggedIn={props.onFavouriteNotLoggedIn}/>
         </div>
     );

63c2348bbe13ca6fd51cadb0fb3440cd068cf54e
Daybreak <andrei1.popescu1@gmail.com>
2023-05-13 23:27:48 +0300

    small changes

--- frontend/components/popupvideo/PopupVideo.tsx
+++ frontend/components/popupvideo/PopupVideo.tsx
@@ -1,50 +1,63 @@
-import React, {useEffect, useRef, useState} from "react";
-import './PopupVideo.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./PopupVideo.css";
 
 interface PopupVideoProps {
-    url?:any;
-    audio?:any;
-    onCardClick:(state:boolean, entityUrl:string, entityAudio:string)=>void;
+    url?: any;
+    audio?: any;
+    onCardClick: (state: boolean, entityUrl: string, entityAudio: string) => void;
 }
 
-const PopupVideo:React.FC<PopupVideoProps> = (props:PopupVideoProps) => {
+const PopupVideo: React.FC<PopupVideoProps> = (props: PopupVideoProps) => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const popupRef = useRef<HTMLDivElement>(null);
     const [isVideoShow, setIsVideoShow] = useState<boolean>(true);
+    const [isAppending, setIsAppending] = useState<boolean>(false);
 
     useEffect(() => {
         const video = videoRef.current;
 
-        // Create a MediaSource object
-        const mediaSource = new MediaSource();
-
-        // Set up the video element to use the MediaSource object as its source
-        if(video!=null) {
+        if (video != null) {
+            const mediaSource = new MediaSource();
             video.src = URL.createObjectURL(mediaSource);
-        }
-        // When the MediaSource object is ready, create a SourceBuffer and append the video and audio sources
-        mediaSource.addEventListener  ('sourceopen', async () => {
-            const sourceBuffer = mediaSource.addSourceBuffer('video/mp4; codecs="avc1.4d401f"');
-            const videoResponse = await fetch(props.url);
-            const videoData = await videoResponse.blob();
-            const arrayBufferVideo = await videoData.arrayBuffer();
-            sourceBuffer.appendBuffer(arrayBufferVideo);
 
-            const audioSourceBuffer = mediaSource.addSourceBuffer('audio/mp4; codecs="mp4a.40.2"');
-            const audioResponse = await fetch(props.audio);
-            const audioData = await audioResponse.blob();
-            const arrayBufferAudio = await audioData.arrayBuffer();
-            audioSourceBuffer.appendBuffer(arrayBufferAudio);
-        });
+            (async () => {
+                try {
+                    const videoSourceBuffer = mediaSource.addSourceBuffer('video/mp4; codecs="avc1.4d401f"');
+                    const audioSourceBuffer = mediaSource.addSourceBuffer('audio/mp4; codecs="mp4a.40.2"');
+                    const videoResponse = await fetch(props.url);
+                    const videoReader = videoResponse.body!.getReader();
+                    const audioResponse = await fetch(props.audio);
+                    const audioReader = audioResponse.body!.getReader();
+
+
+                    while (true) {
+                        const { done: videoDone, value: videoValue } = await videoReader.read();
+                        const { done: audioDone, value: audioValue } = await audioReader.read();
+
+                        if (videoDone && audioDone) {
+                            mediaSource.endOfStream();
+                            break;
+                        }
+
+                        if (!videoDone) {
+                            await videoSourceBuffer.appendBuffer(videoValue!.buffer);
+                        }
+
+                        if (!audioDone) {
+                            await audioSourceBuffer.appendBuffer(audioValue!.buffer);
+                        }
+                    }
+                } catch (err) {
+                    console.error('Error creating media source', err);
+                }
+            })();
+        }
 
         const handleClickOutside = (event: MouseEvent) => {
-            if (
-                popupRef.current &&
-                !popupRef.current.contains(event.target as Node)
-            ) {
-                setIsVideoShow(prevIsVideoShow => {
+            if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+                setIsVideoShow((prevIsVideoShow) => {
                     if (prevIsVideoShow) {
-                        props.onCardClick(false,'','');
+                        props.onCardClick(false, "", "");
                     }
                     return false;
                 });
@@ -58,12 +71,12 @@ const PopupVideo:React.FC<PopupVideoProps> = (props:PopupVideoProps) => {
     }, [props.url, props.audio]);
 
     return (
-      <div className={"popup"}>
-          <div ref={popupRef}>
-          <video ref={videoRef} className={"video-insert-popup"} controls />
-          </div>
-      </div>
+        <div className={"popup"}>
+            <div ref={popupRef}>
+                <video ref={videoRef} className={"video-insert-popup"} controls />
+            </div>
+        </div>
     );
-}
+};
 
 export default PopupVideo;
